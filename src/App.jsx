49e1f4f9f16724908e1040a95d63c1310d8219b3
Taimoor Anwar <77386699+taimoor889@users.noreply.github.com>
2025-06-08 20:17:32 +0200

    added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { allProducts } from "../src/assets/data";
 import CardItems from "./components/CardItems";
+import AddToCart from '../src/components/AddToCart'
 function App() {
- 
   return (
     <>
-      <div className="flex items-center  justify-center bg-gray-100 ">
-        <h1 className="text-5xl mt-8 mb-5">Grab the season's outfit </h1>
+      <div className="flex items-center justify-center bg-gray-100 py-10 px-5">
+        <h1 className="text-5xl mr-6">Grab the season's outfit</h1>
+      <AddToCart/>
       </div>
-      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 xs:grid-cols-1 place-items-center-safe m-16 gap-10">
-       
+      <div className="grid lg:grid-cols-5 sm:grid-cols-3 md:grid-cols-4 xs:grid-cols-2 place-items-center-safe m-16 gap-6">
         {allProducts?.map((prod, index) => (
-          <CardItems prod={prod} key={index}/>
+          <CardItems prod={prod} key={index} />
         ))}
       </div>
     </>
