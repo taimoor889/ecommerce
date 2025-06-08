cd2900a40948acc4c86e3f19bb0f32d674f92a7e
Taimoor Anwar <77386699+taimoor889@users.noreply.github.com>
2025-06-08 20:52:25 +0200

    handle the cart if have the quantity then it should show the numbers....

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -3,10 +3,18 @@ import { useCart } from '../context/CardContext'
 
 
 const CartButtons = ({prod}) => {
-  const  {addToCard}=useCart();
+  const  {addToCard,cardItems,increaseQuantity,decreaseQuantity}=useCart();
+  const itemInCart=cardItems.find((item)=>item.id===prod.id);
   return (
     <div className='absolute top-5 right-5'>
- <button onClick={()=>addToCard(prod)} type ="button" className='text-xs text-white bg-zinc-400 border rounded-md px-2 py-1 hover:bg-zinc-500 cursor-pointer transition-colors duration-200'>+Add Item</button>
+        {!itemInCart ?( <button onClick={()=>addToCard(prod)} type ="button" className='text-xs text-white bg-zinc-400 border rounded-md px-2 py-1 hover:bg-zinc-500 cursor-pointer transition-colors duration-200'>+Add Item</button>)
+        :  <div className="flex items-center gap-2">
+              <button onClick={() => decreaseQuantity(prod.id)} className="px-2 bg-gray-300 rounded">-</button>
+              <span>{itemInCart.quantity}</span>
+              <button onClick={() => increaseQuantity(prod.id)} className="px-2 bg-gray-300 rounded">+</button>
+            </div>
+         }
+
 
     </div>
   )
