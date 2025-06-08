5f0243bdaa64aa9db15c1ea7f5734b262a04b669
Taimoor Anwar <77386699+taimoor889@users.noreply.github.com>
2025-06-08 20:19:22 +0200

    updated card component

--- src/components/CardItems.jsx
+++ src/components/CardItems.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from "react";
+import CartButtons from "./CartButtons";
+import { useCart } from "../context/CardContext";
 
-const CardItems = ({prod,index}) => {
-    const{id, name, imageUrl, price}=prod;
+const CardItems = ({ prod}) => {
+  const { id, name, imageUrl, price } = prod;
+     
   return (
-    <div key={index} className=' relative flex flex-col w-[300px] h-[400px] items-center justify-between  group gap-y-2 border  border-zinc-200 rounded-md p-24'>
-     <img src={imageUrl} width={200}  className=' group-hover:-translate-y-2 transition-all duration-500'/>
+    <div
+      key={id}
+      className=" relative flex flex-col w-[300px] h-[400px] items-center justify-between  group gap-y-2 border  border-zinc-200 rounded-md p-24"
+    >
+      <img
+        src={imageUrl}
+        width={200}
+        className=" group-hover:-translate-y-2 transition-all duration-500"
+      />
 
-  <div className='absolute bottom-5 left-5'>
-<h1 className='text-zinc-700 text-sm'>{name}</h1>
-<span className='text-pink-500 text-sm'>${price}</span>
-
-  </div>
+      <div className="absolute bottom-5 left-5">
+        <h1 className="text-zinc-700 text-sm">{name}</h1>
+        <span className="text-pink-500 text-sm">${price}</span>
+      </div>
+      <CartButtons prod={prod}/>
     </div>
-  )
-}
+  );
+};
 
-export default CardItems
+export default CardItems;
